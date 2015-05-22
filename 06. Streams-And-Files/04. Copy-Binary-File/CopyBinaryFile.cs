0c3b28b9a5956e34c77734a0a128f1903c4508e9
Katya Marincheva <katya.marincheva@outlook.com>
2015-05-22 20:47:47 +0300

    06. Streams and Files Homework

---
+++ 06. Streams-And-Files/04. Copy-Binary-File/CopyBinaryFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class CopyBinaryFile
+{
+    const string NImagePath = "../../text.txt"; 
+    const string DestinationPath = "../../result.txt";
+
+    static void Main()
+    {
+        using (var source = new FileStream(NImagePath, FileMode.Open))
+        {
+            using (var destination = new FileStream(DestinationPath, FileMode.Create))
+            {
+                byte[] buffer = new byte[source.Length];
+                while (true)
+                {
+                    int readBytes = source.Read(buffer, 0, buffer.Length);
+                    if (readBytes == 0)
+                    {
+                        break;
+                    }
+
+                    destination.Write(buffer, 0, readBytes);
+                }
+            }
+        }
+
+        // open image file with results
+        System.Diagnostics.Process.Start(@"..\..\result.jpg");
+    }
+
+    public static Microsoft.Win32.SafeHandles.SafeFileHandle ImagePath { get; set; }
+}
+
