e04f3f84ca4b4c73a60b3c30de57059a0d295918
Katya Marincheva <katya.marincheva@outlook.com>
2015-05-27 20:32:00 +0300

    Exam Preparation C# Advanced

---
+++ Exams and Exam Preparation/Exam-Preparation-Advanced-CSharp/36. Activity-Tracker/ActivityTracker.cs
@@ -0,0 +1,118 @@
+﻿/* Problem 13.	 * Activity Tracker
+This problem is from the Java Basics Exam (3 September 2014). You may check your solution here.
+You are part of the server side application team of brand new and promising activity tracking company. 
+ * Their product "The Spy" is constantly sending data to the server. The data represents the distance walked in meters for every user in format:
+•	24/07/2014 Angel 4600
+•	24/07/2014 Pesho 3200
+•	25/07/2014 Angel 6500
+•	01/08/2014 Pesho 5600
+•	03/08/2014 Ivan 11400
+Write a program to aggregate the data and print the activity of each user per month. 
+ * The months should be represented in ascending order. The users should be ordered alphabetically and the data should be represented 
+ * in the following way: <month>: <user>(<distance>), <user>(<distance>),… For the data above the output should be:
+•	7: Angel(11100), Pesho(3200)
+•	8: Ivan(11400), Pesho(5600)
+Input
+The input comes from the console. At the first line a number n stays which says how many data lines will follow. 
+ * Each of the next n lines holds a data in format <date> <user> <distance>. The input data will always be valid and in the format described. 
+ * There is no need to check it explicitly.
+Output
+Print one line for each month (months are in ascending order). For each month print the users and the sum of distances for each one of them, 
+ * in descending order in format <month>: <user>(<distance>), <user>(<distance>),…
+Constraints
+•	The count of the data lines n is in the range [1…1000].
+•	The <date> is a standard date in format dd/mm/yyyy where dd is the day of the month, 
+ * mm is the numeric representation of the month and yyyy is the full numeric represenation of the year.
+•	The <user> consists of only of Latin characters, with length of [1…20].
+•	The <distance> is floating point number in the range [1…1000].
+•	Time limit: 0.3 sec. Memory limit: 16 MB.
+Example
+Input		
+5
+24/07/2014 Angel 4600
+24/07/2014 Pesho 3200
+25/07/2014 Angel 6500
+01/08/2014 Pesho 5600
+03/08/2014 Ivan 11400	
+ * 
+Output
+7: Angel(11100), Pesho(3200)
+8: Ivan(11400), Pesho(5600)	
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+class ActivityTracker
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("bg-BG");
+        // database
+        SortedDictionary<int, SortedDictionary<string, List<double>>> walkingData = new SortedDictionary<int, SortedDictionary<string, List<double>>>();
+        SortedDictionary<int, SortedSet<string>> finals = new SortedDictionary<int, SortedSet<string>>();
+
+
+        // input
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            // read input
+            string[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int month = DateTime.Parse(input[0]).Month;
+            string user = input[1];
+            double distance = double.Parse(input[2]);
+
+            // in case of new month
+            if (!walkingData.ContainsKey(month))
+            {
+                SortedDictionary<string, List<double>> users = new SortedDictionary<string, List<double>>();
+                List<double> distances = new List<double>();
+
+                distances.Add(distance);
+                users.Add(user, distances);
+                walkingData.Add(month, users);
+            }
+
+            else if (walkingData.ContainsKey(month))
+            {
+                // in case of new user for existing month
+                if (!walkingData[month].ContainsKey(user))
+                {
+                    List<double> distances = new List<double>();
+                    distances.Add(distance);
+
+                    walkingData[month].Add(user, distances);
+                }
+                // in case of new distance for existing user
+                else if (walkingData[month].ContainsKey(user))
+                {
+                    walkingData[month][user].Add(distance);
+                }
+            }
+        }
+
+        foreach (var pair1 in walkingData)
+        {
+            SortedSet<string> usersInfo = new SortedSet<string>();
+            foreach (var pair2 in pair1.Value)
+            {
+                // preparing user info for printing
+                string userInfo = pair2.Key + "(" + pair2.Value.Aggregate((a, b) => b + a) + ")";
+                usersInfo.Add(userInfo);
+            }
+            finals.Add(pair1.Key, usersInfo);
+        }
+
+        // printing
+        foreach (var pair in finals)
+        {
+            Console.WriteLine("{0}: {1}", pair.Key, string.Join(", ", pair.Value));
+        }
+    }
+}
---
+++ Exams and Exam Preparation/Exam-Preparation-Advanced-CSharp/38. Use-Your-Chains-Buddy/UseYourChainsBuddy.cs
@@ -0,0 +1,75 @@
+﻿/* Problem 8.	* Use Your Chains, Buddy
+This problem is from the JavaScript Basics Exam (9 January 2015). You may check your solution here.
+You are in Cherny Vit now and there are 12km to Anchova Bichkiya Hut. You need to get there by car. 
+ * But there is so much snow that you need to use car chains. In order to put them on the wheels correctly, you need to read the manual. But it is encrypted…
+As input you will receive an HTML document as a single string. You need to get the text from all the <p> tags and replace all characters 
+ * which are not small letters and numbers with a space " ". After that you must decrypt the text – 
+ * all letters from a to m are converted to letters from n to z (a  n; b  o; … m  z). The letters from n to z are converted to letters 
+ * from a to m (n  a; o  b; … z  m). All multiple spaces should then be replaced by only one space.
+For example, if we have "<div>Santa</div><p>znahny # grkg ()&^^^&12</p>" we extract "znahny # grkg ()&^^^&12". 
+ * Every character that is not a small letter or a number is replaced with a space ("znahny grkg       12"). 
+ * We convert each small letter as described above ("znahny grkg       12"  "manual text       12") 
+ * and replace all multiple spaces with only one space ("manual text 12"). Finally, we concatenate the decrypted 
+ * text from all <p></p> tags (in this case, it's only one). And there you go – you have the manual ready to read!
+Input
+The input is read from the console and consists of just one line – the string with the HTML document.
+The input data will always be valid and in the format described. There is no need to check it explicitly.
+Output	
+Print on a single line the decrypted text of the manual. See the given examples below.
+Constraints
+•	Allowed working time: 0.2 seconds. Allowed memory: 16 MB.
+Examples
+Input
+<html><head><title></title></head><body><h1>hello</h1><p>znahny!@#%&&&&****</p><div><button>dsad</button></div><p>grkg^^^^%%%)))([]12</p></body></html>
+Output
+manual text 12
+*/
+
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class UseYourChainsBuddy
+{
+    static void Main()
+    {
+        // input
+        Console.SetIn(new StreamReader(Console.OpenStandardInput(8192)));
+        string html = Console.ReadLine();
+
+        // regex patterns
+        string pattern = @"<p>(.[^\/]+)<\/p>";
+        string regex = @"[^a-z0-9]+";
+
+        // logic - collecting the alpanumeric characters
+        Regex words = new Regex(pattern);
+        MatchCollection matches = words.Matches(html);
+        string encrypted = "";
+        for (int i = 0; i < matches.Count; i++)
+        {
+            string temp = matches[i].Groups[1].Value;
+            encrypted += Regex.Replace(temp, regex, word => " ");
+        }
+
+        // decrypting
+        string manual = "";
+        for (int i = 0; i < encrypted.Length; i++)
+        {
+            if (encrypted[i] >= 'a' && encrypted[i] <= 'm')
+            {
+                manual += (char)(encrypted[i] + 13);
+                //Console.WriteLine(manual);
+            }
+            else if (encrypted[i] >= 'n' && encrypted[i] <= 'z')
+            {
+                manual += (char)(encrypted[i] - 13);
+            }
+            else if (Char.IsDigit(encrypted[i]) || Char.IsWhiteSpace(encrypted[i]))
+            {
+                manual += encrypted[i];
+            }
+        }
+        Console.WriteLine(manual);
+    }
+}
+
