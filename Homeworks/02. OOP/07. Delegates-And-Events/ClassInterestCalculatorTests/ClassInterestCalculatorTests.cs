086145e84f0264a36b78046ae0d588b3f5b1d9e7
Katya Marincheva <katya.marincheva@outlook.com>
2015-06-20 11:54:25 +0300

    new Unit Tests added to ClassInterestCalculatorTests.cs

--- Homeworks/02. OOP/07. Delegates-And-Events/ClassInterestCalculatorTests/ClassInterestCalculatorTests.cs
+++ Homeworks/02. OOP/07. Delegates-And-Events/ClassInterestCalculatorTests/ClassInterestCalculatorTests.cs
@@ -26,5 +26,35 @@ namespace ClassInterestCalculatorTests
         {
             InterestCalculator simpleInterest = new InterestCalculator(2500m, 0.072m, 8, InterestCalculatorMain.GetSimpleInterest);
         }
+
+        [TestMethod]
+        public void Test_GetSimpleInterestMethod()
+        {
+            // arrange
+            decimal money = 2500m;
+            decimal interest = 0.072m;
+            int years = 15;
+            decimal expected = 5200.0000m;
+            InterestCalculator simpleInterest = new InterestCalculator(2500m, 0.072m, 15, InterestCalculatorMain.GetSimpleInterest);
+
+            // assert
+            decimal actual = simpleInterest.Balance;
+            Assert.AreEqual(expected, actual, "Interest not calculated correctly");
+        }
+
+        [TestMethod]
+        public void Test_GetCompoundInterestMethod()
+        {
+            // arrange
+            decimal money = 500m;
+            decimal interest = 0.056m;
+            int years = 10;
+            decimal expected = 874.1968m;
+            InterestCalculator compoundInterest = new InterestCalculator(500m, 0.056m, 10, InterestCalculatorMain.GetCompoundInterest);
+
+            // assert
+            decimal actual = compoundInterest.Balance;
+            Assert.AreEqual(expected, actual, "Interest not calculated correctly");
+        }
     }
 }
