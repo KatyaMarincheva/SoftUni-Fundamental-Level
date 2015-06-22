c223e2b875dac424cd2193cd70f7dcd6660c146b
Katya Marincheva <katya.marincheva@outlook.com>
2015-06-22 11:23:36 +0300

    05. OOP-Mass-Effect-Lab

---
+++ Homeworks/02. OOP/05. OOP-Mass-Effect-Lab/MassEffect/Engine/Commands/CreateCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using MassEffect.GameObjects.Enhancements;
+using MassEffect.GameObjects.Ships;
+
+namespace MassEffect.Engine.Commands
+{
+    using MassEffect.Interfaces;
+
+    public class CreateCommand : Command
+    {
+        public CreateCommand(IGameEngine gameEngine) 
+            : base(gameEngine)
+        {
+        }
+
+        public override void Execute(string[] commandArgs)
+        {
+            string type = commandArgs[1];
+            string shipName = commandArgs[2];
+            string locationName = commandArgs[3];
+
+            bool shipAlreadyExists = this.GameEngine.Starships
+                .Any(s => s.Name == shipName);
+
+            if (shipAlreadyExists)
+            {
+                throw new ArgumentException(Messages.DuplicateShipName);
+            }
+
+            var location = this.GameEngine.Galaxy.GetStarSystemByName(locationName);
+            StarshipType shipType = (StarshipType)Enum.Parse(typeof(StarshipType), type);
+
+            var ship = this.GameEngine.ShipFactory.CreateShip(shipType, shipName, location);
+            this.GameEngine.Starships.Add(ship);
+
+            for (int i = 4; i < commandArgs.Length; i++)
+            {
+                var enhancementType = (EnhancementType)
+                    Enum.Parse(typeof(EnhancementType), commandArgs[i]);
+
+                var enhancement = this.GameEngine.EnhancementFactory.Create(enhancementType);
+                ship.AddEnhancement(enhancement);
+            }
+
+            Console.WriteLine(Messages.CreatedShip, shipType, shipName);
+        }
+    }
+}
---
+++ Homeworks/02. OOP/05. OOP-Mass-Effect-Lab/MassEffect/Engine/ExtendedCommandManager.cs
@@ -0,0 +1,14 @@
+﻿using MassEffect.Engine.Commands;
+
+namespace MassEffect.Engine
+{
+    class ExtendedCommandManager : CommandManager
+    {
+        public override void SeedCommands()
+        {
+            base.SeedCommands();
+
+            this.commandsByName["system-report"] = new SystemReportCommand(this.Engine);
+        }
+    }
+}
