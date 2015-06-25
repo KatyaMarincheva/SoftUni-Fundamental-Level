d54c4f2fa6ffa2e30f443d98d13e8fcee3a23e43
Katya Marincheva <katya.marincheva@outlook.com>
2015-06-25 11:54:59 +0300

    Common Type System Exercises

---
+++ 02. OOP/Exercises Collection/Exercises/09. Country/Country.cs
@@ -0,0 +1,187 @@
+﻿namespace _09.Country
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Country : ICloneable, IComparable<Country>
+    {
+        private string name;
+        private long population;
+        private decimal area;
+
+        public Country(string name, long population, decimal area)
+        {
+            this.Name = name;
+            this.Population = population;
+            this.Area = area;
+        }
+
+        public Country(string name, long population, decimal area, HashSet<string> cities)
+            : this(name, population, area)
+        {
+            this.Cities = cities;
+        }
+        
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("name", "Name cannot be empty.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public long Population
+        {
+            get
+            {
+                return this.population;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("population", "Population cannot be negative.");
+                }
+
+                this.population = value;
+            }
+        }
+
+        public decimal Area
+        {
+            get
+            {
+                return this.area;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("area", "Area cannot be negative.");
+                }
+
+                this.area = value;
+            }
+        }
+
+        public HashSet<string> Cities { get; set; }
+
+        public override bool Equals(object param)
+        {
+            // If the cast is invalid, the result will be null
+            Country country = param as Country;
+            // Country country = (Country)param;
+
+            // Check if we have valid not null Student object
+            if (country == null)
+            {
+                return false;
+            }
+
+            // Compare the reference type member fields
+            if (this.Name == country.Name)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public static bool operator ==(Country a, Country b)
+        {
+            if (a == null || b == null)
+            {
+                return false;
+            }
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Country a, Country b)
+        {
+            if (a == null || b == null)
+            {
+                return false;
+            }
+
+            return !a.Equals(b);
+        }
+
+        public object Clone()
+        {
+            return new Country("x", 1, 1)
+            {
+                Name = this.Name,
+                Population = this.Population,
+                Area = this.Area,
+                Cities = new HashSet<string>(this.Cities)
+            };
+        }
+
+        public override string ToString()
+        {
+            string output = string.Format(
+                "{0}: population: {1}, area: {2}", 
+                this.Name, 
+                this.Population, 
+                this.Area);
+
+            if (this.Cities != null)
+            {
+                output += string.Format(", cities: {0}", string.Join(", ", this.Cities));
+            }
+
+            return output;
+        }
+
+        public int CompareTo(Country other)
+        {
+            if (this.Area < other.Area)
+            {
+                return 1;
+            }
+            if (this.Area > other.Area)
+            {
+                return -1;
+            }
+            if (this.Population < other.Population)
+            {
+                return 1;
+            }
+            if (this.Population > other.Population)
+            {
+                return -1;
+            }
+            if (this.Name.CompareTo(other.Name) > 0)
+            {
+                return 1;
+            }
+            if (this.Name.CompareTo(other.Name) < 0)
+            {
+                return -1;
+            }
+            if (this.Name.CompareTo(other.Name) == 0)
+            {
+                return 0;
+            }
+            return 0;
+        }
+    }
+}
---
+++ 02. OOP/Exercises Collection/Exercises/09. Country/CountryMain.cs
@@ -0,0 +1,51 @@
+﻿namespace _09.Country
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountryMain
+    {
+        public static void Main()
+        {
+            Country bg = new Country("Bulgaria", 7100000, 111000, new HashSet<string> { "Sofia", "Plovdiv", "Varna" });
+            Country usa = new Country("USA", 300000000, 1200000, new HashSet<string> { "New York", "Los Angeles", "San Francisco" });
+            Country bg2 = new Country("Bulgaria", 8000000, 10);
+            Country bg3 = new Country("Bulgaria", 8000000, 111000);
+            Country hr = new Country("Croatia", 8000000, 111000);
+
+            // cloning coutries
+            var bgCopy = bg.Clone() as Country;
+            bg.Cities.Add("Kaspichan");
+
+            Console.WriteLine("bg cities: {0}", string.Join(", ", bg.Cities));
+            Console.WriteLine("bgCopy cities: {0}", string.Join(", ", bgCopy.Cities));
+
+            // comparing HashCodes
+            Console.WriteLine("bg.GetHashCode() = {0}", bg.GetHashCode());
+            Console.WriteLine("bg2.GetHashCode() = {0}", bg2.GetHashCode());
+            
+            // comparing countries
+            Console.WriteLine("bg.CompareTo(bgCopy): {0}", bg.CompareTo(bgCopy));
+            Console.WriteLine("bg.CompareTo(usa): {0}", bg.CompareTo(usa));
+
+            //Console.WriteLine(bg.Equals(bg2));
+            //Console.WriteLine(bg == bg2); // True
+            //Console.WriteLine(bg == usa); // False
+            //Console.WriteLine(bg != bg2); // False
+            //Console.WriteLine(bg != usa); // True
+
+            // sorting coutries
+            var countries = new List<Country> { bg, usa, bg2, bg3, hr };
+            countries.Sort();
+
+            Console.WriteLine(
+                string.Join(Environment.NewLine, countries
+                    .Select(c => new { c.Name, c.Area, c.Population })));
+
+            Console.WriteLine(string.Join(Environment.NewLine, countries));
+
+
+        }
+    }
+}
